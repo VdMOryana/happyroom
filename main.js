4e07944b85029a3d2401190ecae397f60cb692e7
Oryana Van de Mosselaer <oryana.vandemosselaer@hotmail.com>
2023-03-22 13:07:54 +0100

    Opdracht 2: natuur maakt gelukkig.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 // Opdracht 1: lachen maakt gelukkig. (Speech functie werkt niet meer?)
 
 const opdracht1 = ['Ha', 'Ha-Ha', 'Ha-Ha-Ha', 'Ha-Ha-Ha-Ha'];
@@ -26,3 +27,25 @@ setTimeout(function () {
   // Start de loop en wacht 5 seconden tussen elke opdracht
   setInterval(runLoop, 6000);
 }, 8000);
+*/
+
+/*
+// Opdracht 2: natuur maakt gelukkig. (nog geluid aan toevoegen? Meer foto's?)
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.opacity = "0";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.opacity = "1";
+  setTimeout(showSlides, 10000); // Change image every 5 seconds
+}
+*/
