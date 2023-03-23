5d1108f4dfc15670fa7bbe7a6e03c21fc0fcc05d
Oryana Van de Mosselaer <oryana.vandemosselaer@hotmail.com>
2023-03-23 13:43:54 +0100

    error

--- main.js
+++ main.js
@@ -49,3 +49,45 @@ function showSlides() {
   setTimeout(showSlides, 10000); // Change image every 5 seconds
 }
 */
+
+import './style.css';
+import * as faceapi from 'face-api.js';
+
+const video = document.querySelector('video');
+
+async function loadModels() {
+  await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
+  await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
+  await faceapi.nets.faceExpressionNet.loadFromUri('/models');
+
+  startVideo();
+}
+
+async function startVideo() {
+  const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+  video.srcObject = stream;
+}
+
+video.addEventListener('play', () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions();
+
+    console.log(detections);
+
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+  }, 100);
+});
+
+loadModels();
