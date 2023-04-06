82279f387ffdbf068a4305851b242ad2692cbde0
Oryana Van de Mosselaer <oryana.vandemosselaer@hotmail.com>
2023-04-06 18:04:42 +0200

    Face detection verwijdert

--- main.js
+++ main.js
@@ -97,77 +97,7 @@ new Swiper('.swiper', {
   slidesPerView: 1, // Set slidesPerView to 1
 });
 
-/* 
-// Face detection 
-import './style.css';
-import * as faceapi from 'face-api.js';
-
-const video = document.querySelector('video');
-
-let hasDetection = false;
-
-async function loadModels() {
-  await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
-  await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
-  await faceapi.nets.faceExpressionNet.loadFromUri('/models');
-
-  startVideo();
-}
-
-async function startVideo() {
-  const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-  video.srcObject = stream;
-}
-
-video.addEventListener('play', () => {
-  const canvasFace = faceapi.createCanvasFromMedia(video);
-  document.body.append(canvasFace);
-  const displaySize = { width: video.width, height: video.height };
-  faceapi.matchDimensions(canvasFace, displaySize);
-
-  // Maak een canvas element en voeg deze toe aan de pagina
-  const canvas = document.createElement('canvas');
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  document.body.appendChild(canvas);
-
-  // Verkrijg een 2D context van de canvas
-  const ctx = canvas.getContext('2d');
-
-  setInterval(async () => {
-    const detections = await faceapi
-      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
-      .withFaceLandmarks()
-      .withFaceExpressions();
-
-    if (detections.length > 0 && !hasDetection) {
-      hasDetection = true;
-
-      // Kies een willekeurige positie en kleur
-      const x = Math.random() * canvas.width;
-      const y = Math.random() * canvas.height;
-      const r = 20; // straal van de bol
-      const color = '#' + Math.floor(Math.random() * 16777215).toString(16); // willekeurige hex kleurcode
-
-      // Teken de bol
-      ctx.beginPath();
-      ctx.arc(x, y, r, 0, 2 * Math.PI);
-      ctx.fillStyle = color;
-      ctx.fill();
-    } else if (detections.length === 0) {
-      hasDetection = false;
-    }
-
-    console.log(detections);
-
-    const resizedDetections = faceapi.resizeResults(detections, displaySize);
-  }, 2000);
-});
-
-loadModels();
-*/
-
-/* loop */
+/* loop 
 const loop = [
   { element: document.getElementById('quote1'), duration: 10000, audio: false },
   {
@@ -251,3 +181,4 @@ function speelOpdrachtenAf() {
 
 // Call the function to start the loop immediately
 speelOpdrachtenAf();
+*/
