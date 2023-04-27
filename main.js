8ae32963971bd39695f84c5f491afd35ef93c2a4
Oryana Van de Mosselaer <oryana.vandemosselaer@hotmail.com>
2023-04-27 14:52:28 +0200

    Afwerken code, vergeten updaten.

--- main.js
+++ main.js
@@ -1,31 +1,38 @@
+// import Swiper bundle with all modules installed
+import Swiper, { Autoplay, EffectFade } from 'swiper';
+// import styles bundle
+import 'swiper/css';
+import 'swiper/css/autoplay';
+import 'swiper/css/effect-fade';
+
 /*
 // Opdracht 1: lachen maakt gelukkig. */
-const opdracht1 = [
-  'Listen and repeat',
-  'Ha',
-  'Ha-Ha',
-  'Ha-Ha-Ha',
-  'Ha-Ha-Ha-Ha',
-];
-let index = 0;
-const opdrachtenElement = document.getElementById('opdrachten');
-let loopInterval;
+function opdracht1func() {
+  const opdracht1 = [
+    'Listen and repeat',
+    'Ha',
+    'Ha-Ha',
+    'Ha-Ha-Ha',
+    'Ha-Ha-Ha-Ha',
+  ];
+  let index = 0;
+  const opdrachtenElement = document.getElementById('opdrachten');
+  let loopInterval;
 
-function runLoop() {
-  const currentOpdracht = opdracht1[index];
-  opdrachtenElement.innerHTML = currentOpdracht;
-  // Speel de tekst af met de SpeechSynthesis API
-  const message = new SpeechSynthesisUtterance(currentOpdracht);
-  message.rate = 0.8;
-  window.speechSynthesis.speak(message);
+  function runLoop() {
+    const currentOpdracht = opdracht1[index];
+    opdrachtenElement.innerHTML = currentOpdracht;
+    // Speel de tekst af met de SpeechSynthesis API
+    const message = new SpeechSynthesisUtterance(currentOpdracht);
+    message.rate = 0.8;
+    window.speechSynthesis.speak(message);
 
-  index++;
-  if (index >= opdracht1.length) {
-    index = 0;
+    index++;
+    if (index >= opdracht1.length) {
+      index = 0;
+    }
   }
-}
 
-setTimeout(function () {
   // Toon de eerste opdracht en verwijder het H1 element
   document.querySelector('h1').style.display = 'none';
 
@@ -36,118 +43,158 @@ setTimeout(function () {
   // Stop de loop na 20 seconden
   setTimeout(function () {
     clearInterval(loopInterval);
-  }, 18000);
-}, 10000);
+  }, loop[1].duration);
+}
 
 /* 
 // Opdracht 2: bewegen maakt gelukkig. */
-
-const video = document.querySelector('video');
-video.addEventListener('ended', function () {
+function opdracht2func() {
+  const video = document.querySelector('video');
   video.currentTime = 0;
   video.play();
-});
+}
 
 /*
 // Opdracht 3: meditatie maakt gelukkig. */
 // Get the HTML elements for the circle
-const circle = document.getElementById('circle');
+function opdracht3func() {
+  const circle = document.getElementById('circle');
 
-// Define a function to animate the circle
-function animateCircle() {
-  // Calculate the scale of the inner circle based on the phase of the animation
-  const maxScale = 1.5;
-  const minScale = 1.0;
-  const isPhase1 = Date.now() % 8000 < 3000; // inhale phase lasts for 4 seconds
-  const phaseProgress = isPhase1
-    ? (Date.now() % 4000) / 4000
-    : (4000 - (Date.now() % 4000)) / 4000; // calculate the progress of the phase
-  const scale = minScale + (maxScale - minScale) * phaseProgress; // calculate the scale based on the progress
+  // Define a function to animate the circle
+  function animateCircle() {
+    // Calculate the scale of the inner circle based on the phase of the animation
+    const maxScale = 2.5;
+    const minScale = 2.0;
+    const isPhase1 = Date.now() % 8000 < 3000; // inhale phase lasts for 4 seconds
+    const phaseProgress = isPhase1
+      ? (Date.now() % 4000) / 4000
+      : (4000 - (Date.now() % 4000)) / 4000; // calculate the progress of the phase
+    const scale = minScale + (maxScale - minScale) * phaseProgress; // calculate the scale based on the progress
 
-  // Set the scale of the inner circle
-  circle.style.transform = `scale(${scale})`;
+    // Set the scale of the inner circle
+    circle.style.transform = `scale(${scale})`;
 
-  // Schedule the next frame of the animation
-  requestAnimationFrame(animateCircle);
+    // Schedule the next frame of the animation
+    if (huidigeOpdracht == 5) {
+      requestAnimationFrame(animateCircle);
+    }
+  }
+
+  Audio5.currentTime = 0;
+  Audio5.autoplay = true;
+  Audio5.loop = true;
+  Audio5.play();
+
+  // Start the animation
+  animateCircle();
 }
-
-// Start the animation
-animateCircle();
 
 /* 
 // Opdracht 4: natuur maakt gelukkig. */
 
-// import Swiper bundle with all modules installed
-import Swiper, { Autoplay, EffectFade } from 'swiper';
-// import styles bundle
-import 'swiper/css';
-import 'swiper/css/autoplay';
-import 'swiper/css/effect-fade';
-
 // init Swiper:
-new Swiper('.swiper', {
-  speed: 25000,
-  effect: 'fade', // Set effect to fade
-  modules: [Autoplay, EffectFade],
-  loop: true,
-  autoplay: {
-    delay: 10000,
-  },
-  direction: 'horizontal', // Set direction to horizontal or vertical
-  slidesPerView: 1, // Set slidesPerView to 1
-});
+let opdracht4swiper = null;
+function opdracht4func() {
+  opdracht4swiper = new Swiper('.swiper', {
+    speed: 25000,
+    effect: 'fade', // Set effect to fade
+    modules: [Autoplay, EffectFade],
+    loop: true,
+    autoplay: {
+      delay: 10000,
+    },
+    direction: 'horizontal', // Set direction to horizontal or vertical
+    slidesPerView: 1, // Set slidesPerView to 1
+  });
 
-/* loop 
+  myAudio.currentTime = 0;
+  myAudio.autoplay = true;
+  myAudio.loop = true;
+  myAudio.play();
+
+  setTimeout(function () {
+    opdracht4swiper.destroy();
+  }, loop[7].duration);
+}
+
+/* loop */
 const loop = [
-  { element: document.getElementById('quote1'), duration: 10000, audio: false },
+  {
+    element: document.getElementById('quote1'),
+    duration: 1000,
+    audio: false,
+    func: null,
+  },
   {
     element: document.getElementById('opdracht1'),
-    duration: 10000,
+    duration: 1000,
     audio: false,
+    func: opdracht1func,
   },
   {
     element: document.getElementById('quote2'),
-    duration: 20000,
+    duration: 1000,
     audio: false,
+    func: null,
   },
   {
     element: document.getElementById('opdracht2'),
-    duration: 10000,
+    duration: 30000,
     audio: false,
+    func: opdracht2func,
   },
   {
     element: document.getElementById('quote3'),
-    duration: 20000,
-    audio: true,
+    duration: 1000,
+    audio: false,
+    func: null,
   },
   {
     element: document.getElementById('opdracht3'),
-    duration: 10000,
+    duration: 1000,
     audio: false,
+    func: opdracht3func,
   },
-  { element: document.getElementById('quote4'), duration: 20000, audio: true },
+  {
+    element: document.getElementById('quote4'),
+    duration: 1000,
+    audio: true,
+    func: null,
+  },
   {
     element: document.getElementById('opdracht4'),
-    duration: 10000,
+    duration: 1000,
     audio: false,
+    func: opdracht4func,
   },
   {
     element: document.getElementById('quote5'),
-    duration: 40000,
+    duration: 1000,
     audio: false,
+    func: null,
   },
   {
     element: document.getElementById('loading'),
-    duration: 10000,
+    duration: 1000,
     audio: false,
+    func: null,
   },
 ];
 
-let huidigeOpdracht = 0;
+let huidigeOpdracht = loop.length;
 const myAudio = new Audio('images/natuurgeluiden.mp3');
 const Audio5 = new Audio('images/meditatie.mp3');
 
 function speelOpdrachtenAf() {
+  huidigeOpdracht++;
+  if (huidigeOpdracht >= loop.length) {
+    huidigeOpdracht = 0;
+  }
+
+  // pause audio
+  myAudio.pause();
+  Audio5.pause();
+
   // Set display of all opdrachten to none
   for (let i = 0; i < loop.length; i++) {
     loop[i].element.style.display = 'none';
@@ -155,24 +202,10 @@ function speelOpdrachtenAf() {
 
   // Set display of current opdracht to block
   loop[huidigeOpdracht].element.style.display = 'block';
-
-  if (huidigeOpdracht === 7) {
-    // Play audio only if current opdracht is opdracht 4
-    myAudio.autoplay = true;
-    myAudio.loop = true;
-    myAudio.play();
-  } else if (huidigeOpdracht === 5) {
-    Audio5.autoplay = true;
-    Audio5.loop = true;
-    Audio5.play();
-  } else {
-    myAudio.pause();
-    Audio5.pause();
-  }
-
-  huidigeOpdracht++;
-  if (huidigeOpdracht >= loop.length) {
-    huidigeOpdracht = 0;
+  console.log(huidigeOpdracht);
+  // Play function
+  if (loop[huidigeOpdracht].func) {
+    loop[huidigeOpdracht].func();
   }
 
   // Set interval for the next opdracht
@@ -181,4 +214,3 @@ function speelOpdrachtenAf() {
 
 // Call the function to start the loop immediately
 speelOpdrachtenAf();
-*/
